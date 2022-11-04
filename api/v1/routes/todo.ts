3adf24026b12e0bde4b6df946b9542dc94fcf509
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 12:09:57 +0800

    feat: add todo route

---
+++ api/v1/routes/todo.ts
@@ -0,0 +1,20 @@
+import { Express } from "express";
+import express from "express";
+import {
+  createTodo,
+  getAllTodo,
+  getOneTodo,
+  updateTodo,
+  deleteTodo,
+} from "../controller/todoController";
+
+const app: Express = express();
+
+app.route("/api/todos").get(getAllTodo).post(createTodo);
+app
+  .route("/api/todos/:id")
+  .get(getOneTodo)
+  .patch(updateTodo)
+  .delete(deleteTodo);
+
+module.exports = app;
