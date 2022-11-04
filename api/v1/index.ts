ea007d21e82729ac73e82b7b3ecbaa3f477d6f52
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 12:09:27 +0800

    feat: add types

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,9 +1,10 @@
+import { Express, Request, Response } from "express";
 import express from "express";
 
-const app = express();
+const app: Express = express();
 // const PORT = 3001;
 
-app.get("/api/home", async (req, res) => {
+app.get("/api/home", async (req: Request, res: Response) => {
   res.json({
     message: "WELCOME!",
   });
