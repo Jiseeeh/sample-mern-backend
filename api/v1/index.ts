fe50a28195bcea091d9b127c01b64bcbf211c8c7
Jiseeeh <johncarlo.camara1@gmail.com>
2022-11-04 12:22:49 +0800

    feat: parse requests and use routes

--- api/v1/index.ts
+++ api/v1/index.ts
@@ -1,8 +1,12 @@
 import { Express, Request, Response } from "express";
 import express from "express";
 
+import todo from "./routes/todo";
+
 const app: Express = express();
 // const PORT = 3001;
+app.use(express.json());
+app.use(todo);
 
 app.get("/api/home", async (req: Request, res: Response) => {
   res.json({
